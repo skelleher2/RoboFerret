dd93998ca208dd3a155d47cca9011e654778dde1
skelleher2 <seankelleher2@gmail.com>
2020-07-08 19:24:20 -0500

    RoboFerret

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './containers/App';
+import serviceWorker from './serviceWorker';
+import 'tachyons';
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
+export default serviceWorker;
+
+
+
