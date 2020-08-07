6af5ec2deec4df83f0a25bd4b4b2c50017c5d116
skelleher2 <seankelleher2@gmail.com>
2020-08-07 17:13:08 -0500

    Origin push to master

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
 import './index.css';
 import App from './containers/App';
 import serviceWorker from './serviceWorker';
+import { searchRobots, requestRobots } from './reducers';
 import 'tachyons';
 
+const logger = createLogger();
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootReducer = combineReducers({ searchRobots, requestRobots})
+const store = 
+    createStore(rootReducer, applyMiddleware(thunkMiddleware, logger))
+
+
+ReactDOM.render(
+    <Provider store={store}> 
+        <App />
+    </Provider>, document.getElementById('root'));
+
 export default serviceWorker;
 
 
